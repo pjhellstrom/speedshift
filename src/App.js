234a717277d1211a517fb0092b82e2b740e87fc1
Eric Jobin <ejobin@gmail.com>
2019-09-05 14:52:42 -0400

    Pushing to git to merge to master

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import About from "./components/pages/About";
+import Discover from "./components/pages/Discover";
+import Search from "./components/pages/Search";
+import APIrand from "./components/APIrand"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		randResults: []
+	};
+
+	randDog = () => {
+		// console.log("In Query")
+		APIrand.search()
+			.then(res => {
+				// console.log(res.data.message);
+				this.setState({ randResults: res.data.message });
+			})
+			// .then(console.log(this.state.results))
+			.catch(err => console.log(err));
+	};
+
+	componentDidMount() {
+		this.randDog();
+	}
+
+	render () {
+		return (
+			<Router>
+				<div>
+					<NavTabs />
+					<Route exact path="/" component={About} />
+					<Route exact path="/discover" component={Discover} 
+						randResults ={this.randResults}
+						search = {this.randDog}
+						
+					/>
+
+
+					<Route exact path="/search" component={Search} />
+				</div>
+			</Router>
+		);
+	}
+
 }
 
 export default App;
