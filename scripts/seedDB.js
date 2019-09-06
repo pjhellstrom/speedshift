0d94b80a0e1ea481e13b60db695c956b939ab3b9
Yating(Esther) Su <yaringsu@foxmail.com>
2019-09-06 10:54:43 -0400

    Add the seedDB file

---
+++ scripts/seedDB.js
@@ -0,0 +1,29 @@
+[
+    {
+        "name" : "Clearning the bath room",
+        "description" : "Hard worker ever",
+        "capacity" : ,
+        "date" : ,
+        "start" : ,
+        "end" : ,
+        "team_id" : 
+    },
+    {
+        "name" : "",
+        "description" : "",
+        "capacity" : ,
+        "date" : ,
+        "start" : ,
+        "end" : ,
+        "team_id" : 
+    },
+    {
+        "name" : "",
+        "description" : "",
+        "capacity" : ,
+        "date" : ,
+        "start" : ,
+        "end" : ,
+        "team_id" : 
+    }
+]
