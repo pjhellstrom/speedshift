ffb02c663a244f9678ebccfa7121c3bb454350b4
Eric Jobin <ejobin@gmail.com>
2019-09-06 14:43:45 -0400

    Have manager main page

---
+++ client/src/components/MgrShift/MgrShift.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import MgrNav from "./MgrNav"
+
+
+class MgrShift extends Component {
+	render() {
+		return (
+			<div>
+				<MgrNav/>
+				<div>
+					<h1>Manager Page</h1>
+					<p>
+						Test
+					</p>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default MgrShift;
