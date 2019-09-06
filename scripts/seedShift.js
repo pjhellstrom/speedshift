c51bbfef8d1ecbdf7727f89913a81ee7d8cfc799
Yating(Esther) Su <yaringsu@foxmail.com>
2019-09-06 14:52:49 -0400

    Change the start and endtype

--- scripts/seedShift.js
+++ scripts/seedShift.js
@@ -5,8 +5,8 @@
         "capacity" : 5,
         "location" :  "124 University Ave",
         "date" : "09-11-2019",
-        "start" : "14:00",
-        "end" : "17:00",
+        "start" : "1400",
+        "end" : "1700",
         "team_id" : 1
     }
 ]
