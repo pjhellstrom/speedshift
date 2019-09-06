aaed525d2e4085c66f3d6c1383857bc888324dbd
Eric Jobin <ejobin@gmail.com>
2019-09-06 11:57:01 -0400

    Added two paths on landing page to bring you to either a manager view page(pending) or employee view page(also pending)

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
-import Home from "./components/pages/Home";
+import LandingPage from "./components/pages/LandingPage";
 import About from "./components/pages/About";
 import Blog from "./components/pages/Blog";
 import Contact from "./components/pages/Contact";
@@ -10,10 +10,10 @@ function App() {
 	return (
 		<Router>
 			<div>
-				<NavTabs />
-				<Route exact path="/" component={Home} />
-				<Route exact path="/about" component={About} />
-				<Route exact path="/blog" component={Blog} />
+				{/* <NavTabs /> */}
+				<Route exact path="/" component={LandingPage} />
+				<Route exact path="/managerview" component={About} />
+				<Route exact path="/employeeview" component={Blog} />
 				<Route path="/contact" component={Contact} />
 			</div>
 		</Router>
