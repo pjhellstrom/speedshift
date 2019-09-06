2378313dc822d250f3cf58392b0a9107ff8e294d
Yating(Esther) Su <yaringsu@foxmail.com>
2019-09-06 11:09:03 -0400

    Completed the seeds files

---
+++ scripts/seedTeam.js
@@ -0,0 +1,6 @@
+[
+    {
+        name: "Coffee line",
+        description: "Tims"
+    }
+]
---
+++ scripts/seedUser.js
@@ -0,0 +1,10 @@
+[
+    {
+        "first_name": "John",
+        "last_name": "Doe",
+        "location": "345 toronto st.",
+        "email": "123@qwe.com",
+        "isManager": true,
+        "team_id": 1
+    }
+]
