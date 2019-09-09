491278e5fd88d6143fbabead0d36783fa53cf5a5
jennyoum <jennyoum@hotmail.com>
2019-09-09 12:15:14 -0400

    added day component

--- client/src/components/Date/Date.js
+++ client/src/components/Date/Date.js
@@ -1,5 +1,5 @@
 import React  from 'react';
-import Moment from 'moment';
+import Moment from 'react-moment';
 import "./style.css";
 
 // class Date extends React.Component {
--- client/src/components/Day/Day.js
+++ client/src/components/Day/Day.js
@@ -1,6 +1,14 @@
-<<<<<<< HEAD
 import React  from 'react';
+import Moment from 'react-moment';
 import "./style.css";
-=======
- 
->>>>>>> master
+import Date from "Date"
+
+ class Day extends React.Component {
+    render() {
+        return (
+            <div><Moment format="YYYY/MM/DD">{this.props.dateToFormat}</Moment></div>
+
+            )
+        }
+    }
+export default Day;
