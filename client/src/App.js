7e6927e8dd573830e327451c0cd26f4418563644
jennyoum <jennyoum@hotmail.com>
2019-09-09 10:42:07 -0400

    changed day and date components

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,8 @@ import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Blog from "./components/pages/Blog";
 import Contact from "./components/pages/Contact";
-import Date from "./components/Date";
-import Day from "./components/Day";
+// import Date from "./components/Date";
+// import Day from "./components/Day";
 
 
 function App() {
--- client/src/components/Date/Date.js
+++ client/src/components/Date/Date.js
@@ -1,10 +1,11 @@
 import React  from 'react';
-import Moment from 'react-moment';
+import Moment from 'moment';
+import "./style.css";
 
-class Date extends React.Component {
-    render() {
-        <Moment format="YYYY/MM/DD">{this.props.dateToFormat}</Moment>
-    }
-}
+// class Date extends React.Component {
+//     render() {
+//         <Moment format="YYYY/MM/DD">{this.props.dateToFormat}</Moment>
+//     }
+// }
 
 export default Date;
