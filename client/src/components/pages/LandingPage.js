d6d11f4aa7a700e65a825f269bb21aeae76e0ead
Zheli Zhu <brianzzl@hotmail.com>
2019-09-09 15:34:58 -0400

    added utils file

--- client/src/components/pages/LandingPage.js
+++ client/src/components/pages/LandingPage.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import API from "../utils/API";
+
+
 
 
 class LandingPage extends Component {
@@ -9,15 +12,15 @@ class LandingPage extends Component {
 				<div>
 					<h1>Landing Page</h1>
 					<p>
-						This will need to be replaced with a proper login page 
+						This will need to be replaced with a proper login page
 					</p>
 				</div>
-				<button className="btn btn-primary"> 
+				<button className="btn btn-primary">
 					<Link to="/managerview" className={window.location.pathname === "/managerview"} style={{color:"white"}}>
 					Login As Manager
 					</Link>
 				</button>
-				<button className="btn btn-primary"> 
+				<button className="btn btn-primary">
 					<Link to="/employeeview" className={window.location.pathname === "/employeeview"} style={{color:"white"}}>
 					Login As Employee
 					</Link>
